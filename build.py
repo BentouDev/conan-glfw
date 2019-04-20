cb30a6c835b6694a30bf97addc8edd44cb344c3c
BentouDev <bentkowski.jakub@gmail.com>
2019-04-20 11:11:38 +0200

    Attempt to fix semver

--- build.py
+++ build.py
@@ -134,9 +134,12 @@ def execute(password):
         if STABLE_IN_GIT:
             channel = 'stable'
         ver_parts = version.split('-1-')
-        if len(ver_parts) > 1 and build_number:
+        if len(ver_parts) > 1:
             # Fix semvar
-            version = ver_parts[0] + '.' + build_number
+            if build_number:
+                version = ver_parts[0] + '.' + build_number
+            else:
+                version = ver_parts[0] + '-dev'
     else:
         channel = 'stable'
 
